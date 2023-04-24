c9c6473033e06ca02fa577c25d98d93b68c8f89e
marco <marco@PCMARCO>
2023-04-24 15:31:12 -0400

    Interfaz Completada Sucesión de Fibonacci Implementada Formateo y reacomodación del codigo

--- src/Ventana/OperacionesVentana.java
+++ src/Ventana/OperacionesVentana.java
@@ -4,6 +4,7 @@ package Ventana;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import static javax.swing.Spring.scale;
 
 public class OperacionesVentana {
@@ -28,20 +29,11 @@ public class OperacionesVentana {
     //Fin M es primo
 
     //Potencia N^M
-//    public static int potRecur(int n, int m) {
-//        if (m == 0) {
-//            return 1;
-//        } else if (m < 0) {
-//            return 1 / potRecur(n, (m * -1));
-//        } else {
-//            return n * potRecur(n, (m - 1));
-//        }
-//    }
     public static BigDecimal potRecur(BigDecimal n1, int n2) {
         if (n2 == 0) {
             return BigDecimal.ONE;
         } else if (n2 < 0) {
-            return BigDecimal.ONE.divide(n1,6,RoundingMode.HALF_UP).multiply(potRecur(n1, n2 + 1));
+            return BigDecimal.ONE.divide(n1, 6, RoundingMode.HALF_UP).multiply(potRecur(n1, n2 + 1));
         } else {
             return n1.multiply(potRecur(n1, n2 - 1));
         }
@@ -49,46 +41,48 @@ public class OperacionesVentana {
     //Fin Potencia N^M
 
     //Fibonacci hasta NxN
-    public static int fibonacci(int n) {
-
-        if (n == 0 || n == 1) { // Si el número es 0 o 1, retorna el mismo número
-            return n;
-        } else { // Si no, llamo recursivamente a la función con n-1 y n-2 y sumo ambos valores
-            return fibonacci(n - 1) + fibonacci(n - 2);
+    public static void fibonacciRecur(int n, ArrayList<Integer> vector, int i) {
+        if (n == 0) {//Si n es igual a 0 solo agregará 0 al vector
+            vector.add(0);
+        } else if (n == 1) {//Si n es igual a 1, agregará solamente 0 y 1
+            vector.add(0);
+            vector.add(1);
+        } else {
+            if (i == 0 || i == 1) { //si el indice i es 0 o 1, colocará el elemento i en la posición i
+                vector.add(i, i);    //Indice,Elemento 
+                fibonacciRecur(n, vector, i + 1);
+            } else {
+                int fib = vector.get(i - 1) + vector.get(i - 2);//Obtiene los 2 valores anteriores a i actual
+                if (fib <= n) {//Si es mayor que n, no se agregará al vector
+                    vector.add(i, fib);
+                    fibonacciRecur(n, vector, i + 1);
+                }
+            }
         }
     }
-    //FinFibonacci hasta NxN
 
-    //Factorial de N
-//    public static int factoRecur(int n) {
-//        if (n < 0) {
-//            return 0;
-//        }
-//        if (n == 0) {
-//            return 1;
-//        } else {
-//            return n * factoRecur(n - 1);
-//        }
-//    }
-     public static BigInteger factoRecur(BigInteger n) {
-
-      if (n.equals(BigInteger.ZERO)) { //Si n es 0, el factorial es 1
-         return BigInteger.ONE;
-      }
-      else { //Si n es mayor que 0, se multiplica por (n-1)!
-         return n.multiply(factoRecur(n.subtract(BigInteger.ONE)));
-      }
-
-   }
-
-    public static String strFactoRecur(int facto, int comp) {
-        if (comp < 0) {
-            return "No se puede obtener el factorial de un número negativo";
-        }
-        if (comp == 0) {
-            return "El factorial de 0 es 1";
+    public static String vectorToString(ArrayList<Integer> vector) {
+        if (vector.isEmpty()) { // si el vector está vacío
+            return ""; // retornar una cadena vacía
         } else {
-            return "El factorial de " + comp + " es " + facto;
+            Integer first = vector.get(0); // obtener el primer elemento del vector
+            vector.remove(0); // remover el primer elemento del vector
+            if (vector.isEmpty()) {
+                return first.toString() + "" + vectorToString(vector);
+            } else {
+                return first.toString() + ", " + vectorToString(vector);
+            }
+        }
+    }
+//FinFibonacci hasta NxN
+
+//Factorial de N
+    public static BigInteger factoRecur(BigInteger n) {
+
+        if (n.equals(BigInteger.ZERO)) { //Si n es 0, el factorial es 1
+            return BigInteger.ONE;
+        } else { //Si n es mayor que 0, se multiplica por (n-1)!
+            return n.multiply(factoRecur(n.subtract(BigInteger.ONE)));
         }
     }
     //Fin Factorial de N
